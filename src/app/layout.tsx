ebb6819003c609f08160bdba9de62a92ee8870ff
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-28 16:17:07 +0000

    removed next init stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Real-Time Web Comms | App",
+  title: "RealTime Web Comms | App",
   description: "Demo application showcasing different communication services, such APIs, SSE, WS, RMQ etc.",
 };
 
