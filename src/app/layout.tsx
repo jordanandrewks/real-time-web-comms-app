3146c4638c84db63d461d4c064be72d85e8900c0
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-29 10:08:52 +0000

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import React, { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -6,17 +7,16 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "RealTime Web Comms | App",
-  description: "Demo application showcasing different communication services, such APIs, SSE, WS, RMQ etc.",
+  description:
+    "Demo application showcasing different communication services, such APIs, SSE, WS, RMQ etc.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>
-      <div>Page</div>
+    <main className="m-5">
+      <h1 className="text-5xl mb-4">WEB COMMS</h1>
+      <p>This is a small web application,</p>
+      <p>for viewing how different web applications work.</p>
+      <br/>
+      <div>
+        <div className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 flex items-center">
+          data1
+        </div>
+        <div>data2</div>
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
