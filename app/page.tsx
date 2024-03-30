9915d77c948d6865a103dd2becdca9911517a405
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-30 17:45:53 +0000

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 import React from 'react';
 import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { AppShell, Button, Burger, Flex } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
 export default function HomePage() {
