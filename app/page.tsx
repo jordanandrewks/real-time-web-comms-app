380abc065b7062c6d5786cbfc787c5612758a76d
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-30 14:45:32 +0000

    home page init

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,38 @@
+'use client';
+import React from 'react';
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import { AppShell, Burger, Flex } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 export default function HomePage() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Flex mih={50} justify="flex-start" align="center" direction="row" wrap="wrap">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+            <div >Logo</div>
+          </Flex>
+        </AppShell.Header>
+
+        <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+        <AppShell.Main>Main</AppShell.Main>
+      </AppShell>
     </>
   );
 }
-
 
 // Header
