95ac78c41123626e14734d35cadee6ecd94cf6ca
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-04-02 10:18:14 +0100

    remove welcome stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Welcome } from '../components/Welcome/Welcome';
 import StageHeader from '@/components/StageHeader/StageHeader';
 
 export default function HomePage() {
   return (
     <>
       <StageHeader title="API STAGE" description="API testing and development" />
-      <Welcome></Welcome>
+
     </>
   );
 }
