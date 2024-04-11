2a806f652b01c324d57e402e8d79450fe3264699
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-04-11 16:21:32 +0100

    Connection process simplified
    If in docker, it will use the mongoEnv, if local it'll use localhost

--- utils/mongo/connectToMongoDb.tsx
+++ utils/mongo/connectToMongoDb.tsx
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
-import { PROJECT_ENVS } from '../projectSettings';
 
 const connection: { isConnected?: number } = {};
 
 async function connectToDb() {
-  if (connection.isConnected) {
-    console.log('connected');
+  const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/realtimewebcomms';
 
+  if (connection.isConnected) {
+    console.log('is connected');
     return;
   }
-  const db = await mongoose.connect(PROJECT_ENVS.MONGO_ENV);
+
+  const db = await mongoose.connect(mongoUri);
   connection.isConnected = db.connections[0].readyState;
-  console.log('not connected');
+  console.log('is not connected');
 }
 export default connectToDb;
--- utils/projectSettings.tsx
+++
@@ -1,6 +0,0 @@
-/* if ever deployed, just turn this off after adding .env ... */
-export const USE_LOCAL_SERVICES = true;
-
-export const PROJECT_ENVS = {
-  MONGO_ENV: USE_LOCAL_SERVICES ? 'mongodb://localhost:27017/realtimewebcomms' : process.env.MONGO as string,
-};
